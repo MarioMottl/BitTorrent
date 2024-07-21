3cb753198debdbe767bc32c9551d3497257199c5
Mario Mottl <mariomottl17@gmail.com>
2024-07-21 17:05:54 +0200

    feat: Add TorrentFile struct for representing torrent file information

--- pkg/torrentfile/torrentfile.go
+++ pkg/torrentfile/torrentfile.go
@@ -6,6 +6,15 @@ import (
 	"github.com/jackpal/bencode-go"
 )
 
+type TorrentFile struct {
+	Announce    string
+	InfoHash    [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	Length      int
+	Name        string
+}
+
 type bencodeInfo struct {
 	Pieces      string `bencode:"pieces"`
 	PieceLength int    `bencode:"piece length"`
